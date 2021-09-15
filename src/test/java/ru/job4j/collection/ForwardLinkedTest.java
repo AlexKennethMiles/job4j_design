392d256f79a12039c7e3b496cb112f29bd538627
AlexKennethMiles <apuchkov17@gmail.com>
2021-09-16 00:20:09 +0300

    4. Using a container based on a linked list, create a Stack container [#71474]

--- src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -31,5 +32,30 @@ public class ForwardLinkedTest {
         assertThat(linked.deleteFirst(), is(1));
         Iterator<Integer> it = linked.iterator();
         assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenDeletingTheLast() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        assertThat(linked.deleteLast(), is(3));
+        assertThat(linked.deleteLast(), is(2));
+        assertThat(linked.deleteLast(), is(1));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenTheChangeDuringTheIteration() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.add(3);
+        assertThat(linked.deleteLast(), is(3));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(linked.deleteLast(), is(2));
+        assertThat(linked.deleteLast(), is(1));
+        assertThat(it.hasNext(), is(false));
     }
 }
