7c9e3e11b522dc9cdc61bfafdcef394ffb43fbe6
AlexKennethMiles <apuchkov17@gmail.com>
2021-09-24 23:29:43 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,69 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index) {
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        if (index == list.size() - 1) {
+            list.add(value);
+        } else {
+            while (i.hasNext()) {
+                if (i.previousIndex() == index) {
+                    i.add(value);
+                    break;
+                }
+                i.next();
+            }
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.set(value);
+            }
+        }
+
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        if (list.size() == 0 || elements.size() == 0) {
+            return;
+        }
+        ListIterator<T> firstI = list.listIterator();
+        while (firstI.hasNext()) {
+            ListIterator<T> secondI = elements.listIterator();
+            T elFI = firstI.next();
+            while (secondI.hasNext()) {
+                if (elFI.equals(secondI.next())) {
+                    firstI.remove();
+                }
+            }
+        }
+    }
+}
