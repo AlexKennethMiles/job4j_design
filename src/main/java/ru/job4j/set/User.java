fde59cc77d467b311c39a62337b62ba0fc2551e9
AlexKennethMiles <apuchkov17@gmail.com>
2021-09-28 23:38:31 +0300

    2. Without overriding equals and hashCode [#1005]

--- src/main/java/ru/job4j/set/User.java
+++ src/main/java/ru/job4j/set/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.set;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,20 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User first = new User("Thomas",
+                2,
+                new GregorianCalendar(1988, Calendar.MARCH, 10)
+        );
+        User second = new User("Thomas",
+                2,
+                new GregorianCalendar(1988, Calendar.MARCH, 10)
+        );
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, new Object());
+        map.put(second, new Object());
+        System.out.println(map);
     }
 }
