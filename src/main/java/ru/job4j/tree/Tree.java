2dff770226d40e108c5d77b192164e22428e9971
AlexKennethMiles <apuchkov17@gmail.com>
2021-10-09 21:32:46 +0300

    1. Create an elementary tree structure [#1711]

---
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,20 @@
+package ru.job4j.tree;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public interface Tree<E> {
+
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final ArrayList<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
