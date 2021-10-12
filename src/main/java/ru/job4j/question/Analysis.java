e366ead36315423bfd2e1ea49ac342a254cd90c6
AlexKennethMiles <apuchkov17@gmail.com>
2021-10-12 22:49:25 +0300

    2.(pre-assembly) Statistics on the collection. [#45889]

---
+++ src/main/java/ru/job4j/question/Analysis.java
@@ -0,0 +1,11 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analysis {
+    public static Info diff(Set<User> previous, Set<User> current) {
+        return null;
+    }
+}
