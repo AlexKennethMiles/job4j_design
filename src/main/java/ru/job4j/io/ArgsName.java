bea285c1257e960b9cc7b1e341b955771ed4116f
AlexKennethMiles <apuchkov17@gmail.com>
2021-10-29 15:23:05 +0300

    5.1.(2) Named Arguments [#295518]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -11,25 +11,37 @@ public class ArgsName {
     }
 
     public void parse(String[] args) {
+        checkThatArgsIsNotNull(args);
+        String[] rsl;
+        for (String arg : args) {
+            checkTemplatesCompliance(arg);
+            rsl = arg.split("=");
+            values.put(rsl[0].substring(1), rsl[1]);
+        }
+    }
+
+    public boolean checkTemplatesCompliance(String arg) {
+        int cursor = arg.indexOf("=");
+        if (cursor == -1 || !arg.startsWith("-")) {
+            throw new IllegalArgumentException("Invalid key value pair. Usage -key=value.");
+        }
+        String key = arg.substring(1, cursor);
+        String value = arg.substring(cursor + 1);
+        if (key.isEmpty()
+                || key.startsWith("-")
+                || key.contains("=")
+                || value.contains("=")
+                || value.isEmpty()) {
+            throw new IllegalArgumentException("Invalid key value pair. Usage -key=value.");
+        }
+        return true;
+    }
+
+    public boolean checkThatArgsIsNotNull(String[] args) {
         if (args.length == 0) {
             throw new IllegalArgumentException("Empty key value pair. Usage -key=value.");
         }
-        for (String arg : args) {
-            int cursor = arg.indexOf("=");
-            if (cursor == -1 || !arg.startsWith("-")) {
-                throw new IllegalArgumentException("Invalid key value pair. Usage -key=value.");
-            }
-            String key = arg.substring(1, cursor);
-            String value = arg.substring(cursor + 1);
-            if (key.isEmpty()
-                    || key.startsWith("-")
-                    || key.contains("=")
-                    || value.contains("=")
-                    || value.isEmpty()) {
-                throw new IllegalArgumentException("Invalid key value pair. Usage -key=value.");
-            }
-            values.put(key, value);
-        }
+        return true;
     }
 
     public static ArgsName of(String[] args) {
