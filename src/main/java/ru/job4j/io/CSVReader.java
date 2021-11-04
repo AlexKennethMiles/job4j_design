62268a81c582643a2a46fb2fc36ef1e3ba76e193
AlexKennethMiles <apuchkov17@gmail.com>
2021-11-04 23:54:52 +0300

    7.(pre-assembly) Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,15 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class CSVReader {
+    public static void handle(ArgsName argsName) {
+
+    }
+}
