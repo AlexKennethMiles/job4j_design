0d4e45fa90d15807abe78853d7032248532efc93
AlexKennethMiles <apuchkov17@gmail.com>
2021-11-18 05:43:18 +0300

    3. Slf4j - output of variables. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,19 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Dima Mayakovsky";
+        int age = 33;
+        char sex = 'M';
+        short height = 185;
+        float visualAcuity = 1.0F;
+        byte numberOfChildren = 2;
+        boolean studentStatus = true;
+        long hoursOfEducation = 19_080L;
+        double distanceCovered = 34_852_221D;
+        LOG.debug("User info name: {}, age: {}, sex: {}, "
+                        + "height: {}, visualAcuity: {}, numberOfChildren: {}, "
+                        + "studentStatus: {}, hoursOfEducation: {}, distanceCovered: {},",
+                name, age, sex, height, visualAcuity, numberOfChildren,
+                studentStatus, hoursOfEducation, distanceCovered);
     }
 }
