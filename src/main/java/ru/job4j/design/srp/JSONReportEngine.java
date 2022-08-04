a041325fcaa48ac5fcbdcb379281f2369e44b348
AlexKennethMiles <apuchkov17@gmail.com>
2022-08-04 15:26:02 +0300

    (fix 2) 1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #328731]

--- src/main/java/ru/job4j/design/srp/JSONReportEngine.java
+++ src/main/java/ru/job4j/design/srp/JSONReportEngine.java
@@ -1,18 +1,21 @@
 package ru.job4j.design.srp;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.function.Predicate;
 
 public class JSONReportEngine implements Report {
     private Store store;
+    private Gson gson;
 
     public JSONReportEngine(Store store) {
         this.store = store;
+        this.gson = new GsonBuilder().create();
     }
 
     @Override
     public String generate(Predicate<Employee> filter) {
-        return new GsonBuilder().create().toJson(store.findBy(filter));
+        return gson.toJson(store.findBy(filter));
     }
 }
